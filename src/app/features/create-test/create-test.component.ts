87a367a821e15cae4a26f6e42ee5e3b8933b9bf2
Mai Trung Tin <trungtin.mai1412@gmail.com>
2023-11-18 16:36:05 +0700

    styles

--- src/app/features/create-test/create-test.component.ts
+++ src/app/features/create-test/create-test.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { Errors } from "src/app/core/models/errors.model";
+import { ListErrorsComponent } from "src/app/shared/list-errors.component";
 
 @Component({
   selector: "app-create-test",
   templateUrl: "./create-test.component.html",
   styleUrls: ["./create-test.component.css"],
+  imports: [ListErrorsComponent],
   standalone: true,
 })
-export class CreateTestComponent {}
+export class CreateTestComponent {
+  isSubmitting = false;
+  errors!: Errors[];
+
+  submitForm() {
+    throw new Error("Method not implemented.");
+  }
+}
