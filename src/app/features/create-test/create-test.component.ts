5b3ef10a5bb0125323d5846b6e2ad7ce9b429917
Mai Trung Tin <trungtin.mai1412@gmail.com>
2023-11-19 09:29:32 +0700

    add model

--- src/app/features/create-test/create-test.component.ts
+++ src/app/features/create-test/create-test.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Errors } from "src/app/core/models/errors.model";
 import { ListErrorsComponent } from "src/app/shared/list-errors.component";
+import { CreateTestForm } from "./CreateTestForm";
 
 @Component({
   selector: "app-create-test",
@@ -12,6 +13,18 @@ import { ListErrorsComponent } from "src/app/shared/list-errors.component";
 export class CreateTestComponent {
   isSubmitting = false;
   errors!: Errors[];
+  test: CreateTestForm = {
+    question: [],
+  };
+
+  constructor() {}
+
+  addQuestion() {
+    this.test.question.push({
+      question: "",
+      answers: [],
+    });
+  }
 
   submitForm() {
     throw new Error("Method not implemented.");
