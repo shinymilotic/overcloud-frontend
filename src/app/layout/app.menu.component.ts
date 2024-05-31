f50b93e98cc1210f700a80d32f9ff39340e2c65e
shinymilotic <trungtin.mai1412@gmail.com>
2024-05-31 23:45:15 +0700

    styles

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,10 +1,24 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { LayoutService } from './service/app.layout.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RippleModule } from 'primeng/ripple';
+import { SidebarModule } from 'primeng/sidebar';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppMenuitemComponent } from './app.menuitem.component';
 
 @Component({
     selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+    templateUrl: './app.menu.component.html',
+    standalone: true,
+    imports: [
+        AppMenuitemComponent
+    ]
 })
 export class AppMenuComponent implements OnInit {
 
