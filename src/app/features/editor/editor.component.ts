d857a83ca57455785769e449cb9e49fbdb4af1a4
shinymilotic <trungtin.mai1412@gmail.com>
2024-06-07 23:05:14 +0700

    editor

--- src/app/features/editor/editor.component.ts
+++ src/app/features/editor/editor.component.ts
@@ -26,6 +26,10 @@ import { Errors } from "../../core/models/errors.model";
 import { LiveAnnouncer } from "@angular/cdk/a11y";
 import { TagsService } from "src/app/core/services/tags.service";
 import { SideBarComponent } from "../side-bar/side-bar.component";
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+import {Paragraph} from '@tiptap/extension-paragraph'
+import {Heading} from '@tiptap/extension-heading'
 
 interface ArticleForm {
   title: FormControl<string>;
@@ -47,11 +51,11 @@ interface ArticleForm {
         NgFor,
         AsyncPipe,
         FormsModule,
-        SideBarComponent
+        SideBarComponent,
+        
     ]
 })
 export class EditorComponent implements OnInit, OnDestroy {
-  @HostBinding("class") classes = "app-editor-page";
   articleForm: FormGroup<ArticleForm>;
   tagField: FormControl<string>;
   errors!: Errors[];
@@ -69,7 +73,8 @@ export class EditorComponent implements OnInit, OnDestroy {
   isInputTag: boolean = true;
   selectedTagIndex: number;
   activeElement: Element | null = null;
-
+  editor!: Editor;
+  items: Array<any> = [];
   constructor(
     private readonly articleService: ArticlesService,
     private readonly route: ActivatedRoute,
@@ -99,15 +104,136 @@ export class EditorComponent implements OnInit, OnDestroy {
     }
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.allTags.filter((tag) =>
-      tag.toLowerCase().includes(filterValue)
-    );
-  }
-
   ngOnInit() {
+    this.editor = new Editor({
+      element: document.querySelector('.tiptap-editor') as HTMLElement,
+      extensions: [
+        StarterKit,
+        
+      ],
+      content: '',
+
+    });
+
+    this.items = [
+      {
+        icon: 'bold',
+        title: 'Bold',
+        action: () => this.editor.chain().focus().toggleBold().run(),
+        isActive: () => this.editor.isActive('bold'),
+      },
+      {
+        icon: 'italic',
+        title: 'Italic',
+        action: () => this.editor.chain().focus().toggleItalic().run(),
+        isActive: () => this.editor.isActive('italic'),
+      },
+      {
+        icon: 'strikethrough',
+        title: 'Strike',
+        action: () => this.editor.chain().focus().toggleStrike().run(),
+        isActive: () => this.editor.isActive('strike'),
+      },
+      {
+        icon: 'code-view',
+        title: 'Code',
+        action: () => this.editor.chain().focus().toggleCode().run(),
+        isActive: () => this.editor.isActive('code'),
+      },
+      // {
+      //   icon: 'mark-pen-line',
+      //   title: 'Highlight',
+      //   action: () => this.editor.chain().focus().toggleHighlight().run(),
+      //   isActive: () => this.editor.isActive('highlight'),
+      // },
+      {
+        type: 'divider',
+      },
+      {
+        icon: 'h-1',
+        title: 'Heading 1',
+        action: () => this.editor.chain().focus().toggleHeading({ level: 1 }).run(),
+        isActive: () => this.editor.isActive('heading', { level: 1 }),
+      },
+      {
+        icon: 'h-2',
+        title: 'Heading 2',
+        action: () => this.editor.chain().focus().toggleHeading({ level: 2 }).run(),
+        isActive: () => this.editor.isActive('heading', { level: 2 }),
+      },
+      {
+        icon: 'paragraph',
+        title: 'Paragraph',
+        action: () => this.editor.chain().focus().setParagraph().run(),
+        isActive: () => this.editor.isActive('paragraph'),
+      },
+      {
+        icon: 'list-unordered',
+        title: 'Bullet List',
+        action: () => this.editor.chain().focus().toggleBulletList().run(),
+        isActive: () => this.editor.isActive('bulletList'),
+      },
+      {
+        icon: 'list-ordered',
+        title: 'Ordered List',
+        action: () => this.editor.chain().focus().toggleOrderedList().run(),
+        isActive: () => this.editor.isActive('orderedList'),
+      },
+      // {
+      //   icon: 'list-check-2',
+      //   title: 'Task List',
+      //   action: () => this.editor.chain().focus().toggleTaskList().run(),
+      //   isActive: () => this.editor.isActive('taskList'),
+      // },
+      {
+        icon: 'code-box-line',
+        title: 'Code Block',
+        action: () => this.editor.chain().focus().toggleCodeBlock().run(),
+        isActive: () => this.editor.isActive('codeBlock'),
+      },
+      {
+        type: 'divider',
+      },
+      {
+        icon: 'double-quotes-l',
+        title: 'Blockquote',
+        action: () => this.editor.chain().focus().toggleBlockquote().run(),
+        isActive: () => this.editor.isActive('blockquote'),
+      },
+      {
+        icon: 'separator',
+        title: 'Horizontal Rule',
+        action: () => this.editor.chain().focus().setHorizontalRule().run(),
+      },
+      {
+        type: 'divider',
+      },
+      {
+        icon: 'text-wrap',
+        title: 'Hard Break',
+        action: () => this.editor.chain().focus().setHardBreak().run(),
+      },
+      {
+        icon: 'format-clear',
+        title: 'Clear Format',
+        action: () => this.editor.chain().focus().clearNodes().unsetAllMarks()
+          .run(),
+      },
+      {
+        type: 'divider',
+      },
+      {
+        icon: 'arrow-go-back-line',
+        title: 'Undo',
+        action: () => this.editor.chain().focus().undo().run(),
+      },
+      {
+        icon: 'arrow-go-forward-line',
+        title: 'Redo',
+        action: () => this.editor.chain().focus().redo().run(),
+      },
+    ];
+
     const slug = this.route.snapshot.params["slug"];
     if (slug != undefined) {
       combineLatest([
